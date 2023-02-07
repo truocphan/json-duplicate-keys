ca5c6970aa561e2b02005fabd0304ff1d0b5b8b4
Truoc Phan <57470560+truocphan@users.noreply.github.com>
2023-02-07 11:53:44 +0700

    Update __init__.py

--- json_duplicate_keys/__init__.py
+++ json_duplicate_keys/__init__.py
@@ -6,54 +6,62 @@
 
 <<< OUTPUT: {'author': 'truocphan', 'version': '22.3.3', 'version{{{_2_}}}': 'latest', 'release': [{'version': '22.3.3', 'version{{{_2_}}}': 'latest'}], 'snapshot': {'author': 'truocphan', 'version': '22.3.3', 'release': [{'version': 'latest'}]}}
 """
-def loads(Jstr, dupSign_start="{", dupSign_end="}", _isDebug_=False):
+def loads(Jstr, dupSign_start="{", dupSign_end="}", ordered_dict=False, _isDebug_=False):
 	import json
 	import re
 	from collections import OrderedDict
 	import traceback
 
 	def __convert_Jloads_to_Jobj(Jloads, Jobj):
-		if type(Jloads) == OrderedDict:
+		if type(Jloads) in [dict, OrderedDict]:
 			for k in Jloads.keys():
 				_key = re.split(dupSign_start_escape_regex*3+"_\d+_"+dupSign_end_escape_regex*3+"$", k)[0]
 
 				if _key not in Jobj.keys():
-					if type(Jloads[k]) not in [list, OrderedDict]:
+					if type(Jloads[k]) not in [list, dict, OrderedDict]:
 						Jobj[_key] = Jloads[k]
 					else:
 						if type(Jloads[k]) == list:
 							Jobj[_key] = list()
+						elif type(Jloads[k]) == dict:
+							Jobj[_key] = dict()
 						else:
 							Jobj[_key] = OrderedDict()
 
 						__convert_Jloads_to_Jobj(Jloads[k], Jobj[_key])
 				else:
 					countObj = len([i for i in Jobj.keys() if _key==re.split(dupSign_start_escape_regex*3+"_\d+_"+dupSign_end_escape_regex*3+"$", i)[0]])
-					if type(Jloads[k]) not in [list, OrderedDict]:
+					if type(Jloads[k]) not in [list, dict, OrderedDict]:
 						Jobj[_key+dupSign_start*3+"_"+str(countObj+1)+"_"+dupSign_end*3] = Jloads[k]
 					else:
 						if type(Jloads[k]) == list:
 							Jobj[_key+dupSign_start*3+"_"+str(countObj+1)+"_"+dupSign_end*3] = list()
+						elif type(Jloads[k]) == dict:
+							Jobj[_key+dupSign_start*3+"_"+str(countObj+1)+"_"+dupSign_end*3] = dict()
 						else:
 							Jobj[_key+dupSign_start*3+"_"+str(countObj+1)+"_"+dupSign_end*3] = OrderedDict()
 
 						__convert_Jloads_to_Jobj(Jloads[k], Jobj[_key+dupSign_start*3+"_"+str(countObj+1)+"_"+dupSign_end*3])
 		elif type(Jloads) == list:
 			for i in range(len(Jloads)):
-				if type(Jloads[i]) not in [list, OrderedDict]:
+				if type(Jloads[i]) not in [list, dict, OrderedDict]:
 					Jobj.append(Jloads[i])
 				else:
 					if type(Jloads[i]) == list:
 						Jobj.append(list())
+					elif type(Jloads[i]) == dict:
+						Jobj.append(dict())
 					else:
 						Jobj.append(OrderedDict())
 
 					__convert_Jloads_to_Jobj(Jloads[i], Jobj[i])
 
 	try:
-		Jloads = json.loads(Jstr, object_pairs_hook=OrderedDict)
+		Jloads = json.loads(Jstr)
+		if ordered_dict:
+			Jloads = json.loads(Jstr, object_pairs_hook=OrderedDict)
 
-		if type(Jloads) in [list, OrderedDict] and len(Jloads) > 0:
+		if type(Jloads) in [list, dict, OrderedDict] and len(Jloads) > 0:
 			try:
 				if type(dupSign_start) not in [str, unicode] or len(dupSign_start) == 0: dupSign_start = "{"
 			except Exception as e:
@@ -92,10 +100,14 @@ def loads(Jstr, dupSign_start="{", dupSign_end="}", _isDebug_=False):
 			Jstr = re.sub('\x02\x03', r'\\"', Jstr)
 			Jstr = re.sub('\x04\x05', r'"', Jstr)
 
-			Jloads = json.loads(Jstr, object_pairs_hook=OrderedDict)
+			Jloads = json.loads(Jstr)
+			if ordered_dict:
+				Jloads = json.loads(Jstr, object_pairs_hook=OrderedDict)
 
 			if type(Jloads) == list:
 				Jobj = list()
+			elif type(Jloads) == dict:
+				Jobj = dict()
 			else:
 				Jobj = OrderedDict()
 
@@ -120,7 +132,7 @@ def loads(Jstr, dupSign_start="{", dupSign_end="}", _isDebug_=False):
 # # # # # # # # # # # # # # # # # # # # # # #
 """
 """
-def load(Jfilepath, dupSign_start="{", dupSign_end="}", _isDebug_=False):
+def load(Jfilepath, dupSign_start="{", dupSign_end="}", ordered_dict=False, _isDebug_=False):
 	import traceback
 
 	try:
@@ -131,7 +143,7 @@ def load(Jfilepath, dupSign_start="{", dupSign_end="}", _isDebug_=False):
 		if _isDebug_: traceback.print_exc()
 		return None
 
-	return loads(Jstr, dupSign_start=dupSign_start, dupSign_end=dupSign_end, _isDebug_=_isDebug_)
+	return loads(Jstr, dupSign_start=dupSign_start, dupSign_end=dupSign_end, ordered_dict=ordered_dict, _isDebug_=_isDebug_)
 # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # #
@@ -225,16 +237,20 @@ def dump(Jobj, Jfilepath, dupSign_start="{", dupSign_end="}", _isDebug_=False, s
 
 <<< OUTPUT: {'author': 'truocphan', 'version': '22.3.3', 'version{{{_2_}}}': '22.3.14', 'release||$0$||version': '22.3.3', 'release||$0$||version{{{_2_}}}': 'latest', 'snapshot||author': 'truocphan', 'snapshot||version': '22.3.3', 'snapshot||release||$0$||version': 'latest'}
 """
-def flatten(Jobj, separator="||", parse_index="$", _isDebug_=False):
+def flatten(Jobj, separator="||", parse_index="$", ordered_dict=False, _isDebug_=False):
 	from collections import OrderedDict
 	import traceback
 
-	Jflat = OrderedDict()
+	Jflat = dict()
+	if ordered_dict:
+		Jflat = OrderedDict()
 
 	def __convert_Jobj_to_Jflat(Jobj, key=None):
 		if type(Jobj) in [dict, OrderedDict]:
 			if len(Jobj) == 0:
-				Jflat[key] = OrderedDict()
+				Jflat[key] = dict()
+				if ordered_dict:
+					Jflat[key] = OrderedDict()
 			else:
 				for k,v in Jobj.items():
 					_Jobj = v
@@ -297,7 +313,7 @@ def flatten(Jobj, separator="||", parse_index="$", _isDebug_=False):
 
 <<< OUTPUT: {'author': 'truocphan', 'version': '22.3.3', 'version{{{_2_}}}': '22.3.14', 'release': [{'version': '22.3.3', 'version{{{_2_}}}': 'latest'}], 'snapshot': {'author': 'truocphan', 'version': '22.3.3', 'release': [{'version': '22.3.14'}]}}
 """
-def unflatten(Jflat, separator="||", parse_index="$", _isDebug_=False):
+def unflatten(Jflat, separator="||", parse_index="$", ordered_dict=False, _isDebug_=False):
 	import re
 	from collections import OrderedDict
 	import traceback
@@ -305,16 +321,18 @@ def unflatten(Jflat, separator="||", parse_index="$", _isDebug_=False):
 	try:
 		if type(Jflat) in [dict, OrderedDict]:
 			if len(Jflat) == 0:
-				return OrderedDict()
+				if ordered_dict:
+					return OrderedDict()
+				return dict()
 			else:
-				Jobj = list() if len([k for k in Jflat.keys() if re.compile("^"+re.escape(parse_index)+"\d+"+re.escape(parse_index)+"$").match(str(k).split(separator)[0])]) == len(Jflat.keys()) else OrderedDict()
+				Jobj = list() if len([k for k in Jflat.keys() if re.compile("^"+re.escape(parse_index)+"\d+"+re.escape(parse_index)+"$").match(str(k).split(separator)[0])]) == len(Jflat.keys()) else OrderedDict() if ordered_dict else dict()
 
 				for k, v in Jflat.items():
 					Jtmp = Jobj
 					Jkeys = k.split(separator)
 
 					for count, (Jkey, next_Jkeys) in enumerate(zip(Jkeys, Jkeys[1:] + [v]), 1):
-						v = next_Jkeys if count == len(Jkeys) else list() if re.compile("^"+re.escape(parse_index)+"\d+"+re.escape(parse_index)+"$").match(next_Jkeys) else OrderedDict()
+						v = next_Jkeys if count == len(Jkeys) else list() if re.compile("^"+re.escape(parse_index)+"\d+"+re.escape(parse_index)+"$").match(next_Jkeys) else OrderedDict() if ordered_dict else dict()
 
 						if type(Jtmp) == list:
 							Jkey = int(re.compile(re.escape(parse_index)+"(\d+)"+re.escape(parse_index)).match(Jkey).group(1))
